13703169a97294d37a7cd34e81f8f373d56434f2
Anthonybm8 <anthony.monta2291@utc.edu.ec>
2025-05-21 15:14:15 -0500

    se creo el modelo paisaje sonoro y las importaciones de empleado y espacio en models.py

--- Aplicaciones/Paisaje_sonoro/models.py
+++ Aplicaciones/Paisaje_sonoro/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from Aplicaciones.Empleado.models import Empleado
+from Aplicaciones.Espacio.models import Espacio
 
 # Create your models here.
+class PaisajeSonoro(models.Model):
+    titulo = models.CharField(max_length=100)
+    fecha_creacion = models.DateField()
+    duracion = models.DurationField()
+    descripcion = models.TextField(blank=True)
+    
+    artista = models.ForeignKey(Empleado)
+    espacio = models.ForeignKey(Espacio)
