8af22f9533bde49b23cbd3dd1f4989e74f8462fd
Anthonybm8 <anthony.monta2291@utc.edu.ec>
2025-05-21 15:02:38 -0500

    se creo el modelo empleado en models.py

--- Aplicaciones/Empleado/models.py
+++ Aplicaciones/Empleado/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    tipo_empleado = models.CharField(max_length=50)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} ({self.tipo_empleado})"
