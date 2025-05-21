517ea9918f817f4676a5500f103986dc10dd3ad5
Anthonybm8 <anthony.monta2291@utc.edu.ec>
2025-05-21 15:09:35 -0500

    se creo el modelo espacio en models.py

--- Aplicaciones/Espacio/models.py
+++ Aplicaciones/Espacio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Espacio(models.Model):
+    nombre = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=50)
+    ubicacion = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.tipo}"
