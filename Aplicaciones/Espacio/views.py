bb60cb8d9a410624000a40423300626243ad7140
Anthonybm8 <anthony.monta2291@utc.edu.ec>
2025-05-21 21:55:46 -0500

    Se agrego views a espacios

--- Aplicaciones/Espacio/views.py
+++ Aplicaciones/Espacio/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Espacio
+from django.contrib import messages
 
 # Create your views here.
+def espacio(request):
+    lista_espacios = Espacio.objects.all()
+    return render(request, 'espacio.html', {'lista_espacios': lista_espacios})
